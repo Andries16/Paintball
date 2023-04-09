b7f76cb0c5e7b4a531ece59b9d4aced665f02233
Andries16 <aaw2012sirbu.andrei@gmail.com>
2023-04-09 20:54:31 +0300

    gh-pages

---
+++ client/src/components/Header.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+const Header = ()=>{
+    return (
+
+        <header className="top_panel top_panel_custom top_panel_custom_42 top_panel_custom_header-fullwidth-simple without_bg_image scheme_dark">
+          <div className="vc_row wpb_row vc_row-fluid vc_custom_1494938325691 vc_row-has-fill vc_row-o-equal-height vc_row-o-content-middle vc_row-flex sc_layouts_row sc_layouts_row_type_compact sc_layouts_row_fixed scheme_dark">
+            <div className="wpb_column vc_column_container vc_col-sm-2 vc_col-has-fill sc_layouts_column sc_layouts_column_align_center sc_layouts_column_icons_position_left">
+              <div className="vc_column-inner vc_custom_1495020648352">
+                <div className="wpb_wrapper">
+                  <div className="sc_layouts_item"><a href="#" id="sc_layouts_logo_1999158379" className="sc_layouts_logo sc_layouts_logo_default  vc_custom_1495020686119"><img className="logo_image" src="https://adrena.ancorathemes.com/wp-content/uploads/2017/05/logo_black.png" alt="" width={183} height={38} /></a></div>
+                </div>
+              </div>
+            </div>
+            <div className="wpb_column vc_column_container vc_col-sm-7 vc_col-has-fill sc_layouts_column sc_layouts_column_align_left sc_layouts_column_icons_position_left">
+              <div className="vc_column-inner vc_custom_1495020517465">
+                <div className="wpb_wrapper">
+                  <div className="sc_layouts_item">
+                    <nav id="sc_layouts_menu_1002075493" className="sc_layouts_menu sc_layouts_menu_default menu_hover_fade hide_on_mobile vc_custom_1494941624072 inited" data-animation-in="fadeInUpSmall" data-animation-out="fadeOutDownSmall">
+                      <ul id="menu-main-menu" className="sc_layouts_menu_nav inited sf-js-enabled sf-arrows">
+                        <li id="menu-item-111" className="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-111">
+                          <a href="#" className="sf-with-ul"><span>Home</span></a>
+                          <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                            <li id="menu-item-116" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-93 current_page_item menu-item-116"><a href="https://adrena.ancorathemes.com/" aria-current="page"><span>Home 1</span></a></li>
+                            <li id="menu-item-671" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-671"><a href="https://adrena.ancorathemes.com/boxed-home/"><span>Home 2 (Boxed)</span></a></li>
+                          </ul>
+                        </li>
+                        <li id="menu-item-121" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-121">
+                          <a href="#" className="sf-with-ul"><span>Features</span></a>
+                          <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                            <li id="menu-item-123" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-123">
+                              <a href="#" className="sf-with-ul"><span>Pages</span></a>
+                              <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                <li id="menu-item-155" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-155">
+                                  <a href="#" className="sf-with-ul"><span>Blog</span></a>
+                                  <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                    <li id="menu-item-143" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-143">
+                                      <a href="#" className="sf-with-ul"><span>Classic</span></a>
+                                      <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                        <li id="menu-item-153" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-153"><a href="https://adrena.ancorathemes.com/classic-1-column/"><span>1 column</span></a></li>
+                                        <li id="menu-item-152" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-152"><a href="https://adrena.ancorathemes.com/classic-2-columns/"><span>2 columns</span></a></li>
+                                        <li id="menu-item-151" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-151"><a href="https://adrena.ancorathemes.com/classic-3-columns/"><span>3 columns</span></a></li>
+                                      </ul>
+                                    </li>
+                                    <li id="menu-item-144" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-144">
+                                      <a href="#" className="sf-with-ul"><span>Chess</span></a>
+                                      <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                        <li id="menu-item-150" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-150"><a href="https://adrena.ancorathemes.com/chess-2-columns/"><span>2 columns</span></a></li>
+                                        <li id="menu-item-149" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-149"><a href="https://adrena.ancorathemes.com/chess-4-columns/"><span>4 columns</span></a></li>
+                                        <li id="menu-item-148" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-148"><a href="https://adrena.ancorathemes.com/chess-6-columns/"><span>6 columns</span></a></li>
+                                      </ul>
+                                    </li>
+                                    <li id="menu-item-154" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-154">
+                                      <a href="#" className="sf-with-ul"><span>Portfolio</span></a>
+                                      <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                        <li id="menu-item-147" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a href="https://adrena.ancorathemes.com/portfolio-2-columns/"><span>2 columns</span></a></li>
+                                        <li id="menu-item-146" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a href="https://adrena.ancorathemes.com/portfolio-3-columns/"><span>3 columns</span></a></li>
+                                        <li id="menu-item-145" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-145"><a href="https://adrena.ancorathemes.com/portfolio-4-columns/"><span>4 columns</span></a></li>
+                                      </ul>
+                                    </li>
+                                  </ul>
+                                </li>
+                                <li id="menu-item-171" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-171">
+                                  <a href="#" className="sf-with-ul"><span>Gallery</span></a>
+                                  <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                    <li id="menu-item-174" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-174"><a href="https://adrena.ancorathemes.com/grid/"><span>Grid</span></a></li>
+                                    <li id="menu-item-172" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-172"><a href="https://adrena.ancorathemes.com/masonry/"><span>Masonry</span></a></li>
+                                    <li id="menu-item-173" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-173"><a href="https://adrena.ancorathemes.com/cobbles/"><span>Cobbles</span></a></li>
+                                  </ul>
+                                </li>
+                                <li id="menu-item-255" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-255"><a href="https://adrena.ancorathemes.com/shop/"><span>Store</span></a></li>
+                                <li id="menu-item-119" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-119"><a href="https://adrena.ancorathemes.com/vouchers/"><span>Vouchers</span></a></li>
+                              </ul>
+                            </li>
+                            <li id="menu-item-316" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-316">
+                              <a href="#" className="sf-with-ul"><span>Tools</span></a>
+                              <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                <li id="menu-item-315" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-315"><a href="https://adrena.ancorathemes.com/typography/"><span>Typography</span></a></li>
+                                <li id="menu-item-314" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-314"><a href="https://adrena.ancorathemes.com/shortcodes/"><span>Shortcodes</span></a></li>
+                              </ul>
+                            </li>
+                            <li id="menu-item-317" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-317">
+                              <a href="#" className="sf-with-ul"><span>Help</span></a>
+                              <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                                <li id="menu-item-951" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-951"><a href="https://adrena.ancorathemes.com/service-plus/"><span>Service Plus</span></a></li>
+                                <li id="menu-item-765" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-765"><a href="https://adrena.ancorathemes.com/privacy-policy/"><span>Privacy Policy</span></a></li>
+                              </ul>
+                            </li>
+                          </ul>
+                        </li>
+                        <li id="menu-item-114" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-114"><a href="https://adrena.ancorathemes.com/book-now/"><span>Book Now</span></a></li>
+                        <li id="menu-item-120" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-120"><a href="https://adrena.ancorathemes.com/zones/"><span>Zones</span></a></li>
+                        <li id="menu-item-118" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-118"><a href="https://adrena.ancorathemes.com/prices/"><span>Prices</span></a></li>
+                        <li id="menu-item-122" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-122">
+                          <a href="#" className="sf-with-ul"><span>About</span></a>
+                          <ul className="sub-menu animated fast fadeOutDownSmall" style={{display: 'none'}}>
+                            <li id="menu-item-112" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-112"><a href="https://adrena.ancorathemes.com/about-us-style-1/"><span>About Us Style 1</span></a></li>
+                            <li id="menu-item-113" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-113"><a href="https://adrena.ancorathemes.com/about-us-style-2/"><span>About Us Style 2</span></a></li>
+                          </ul>
+                        </li>
+                        <li id="menu-item-115" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-115"><a href="https://adrena.ancorathemes.com/contacts/"><span>Contacts</span></a></li>
+                        <li id="menu-item-117" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-117"><a href="https://adrena.ancorathemes.com/offers/"><span>Offers</span></a></li>
+                        <li className="menu-item menu-collapse" style={{display: 'none'}}>
+                          <a href="#" className="sf-with-ul trx_addons_icon-dot3" />
+                          <ul className="submenu" />
+                        </li>
+                      </ul>
+                    </nav>
+                    <div className="sc_layouts_iconed_text sc_layouts_menu_mobile_button">
+                      <a className="sc_layouts_item_link sc_layouts_iconed_text_link" href="#">
+                        <span className="sc_layouts_item_icon sc_layouts_iconed_text_icon trx_addons_icon-menu" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="wpb_column vc_column_container vc_col-sm-3 vc_col-has-fill sc_layouts_column sc_layouts_column_align_center sc_layouts_column_icons_position_left">
+              <div className="vc_column-inner vc_custom_1496326619518">
+                <div className="wpb_wrapper">
+                  <div className="sc_layouts_item">
+                    <div id="sc_layouts_iconed_text_1196024258" className="sc_layouts_iconed_text"><a href="tel:1234567890" className="sc_layouts_item_link sc_layouts_iconed_text_link"><span className="sc_layouts_item_icon sc_layouts_iconed_text_icon icon-phone-1" /><span className="sc_layouts_item_details sc_layouts_iconed_text_details"><span className="sc_layouts_item_details_line1 sc_layouts_iconed_text_line1">123-456-7890</span></span></a></div>
+                  </div>
+                  <div className="sc_layouts_item sc_layouts_hide_on_tablet">
+                    <div id="sc_socials_1330827684" className="sc_socials hide_on_tablet sc_socials_default">
+                      <div className="socials_wrap"><a target="_blank" href="https://twitter.com/themes_ancora" className="social_item social_item_style_icons social_item_type_icons"><span className="social_icon social_twitter"><span className="icon-twitter" /></span></a><a target="_blank" href="https://www.facebook.com/AncoraThemes/" className="social_item social_item_style_icons social_item_type_icons"><span className="social_icon social_facebook"><span className="icon-facebook" /></span></a><a target="_blank" href="#" className="social_item social_item_style_icons social_item_type_icons"><span className="social_icon social_rss"><span className="icon-rss" /></span></a></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sc_layouts_row_fixed_placeholder" style={{backgroundColor: 'rgb(28, 32, 26)'}} />
+          <div className="vc_row wpb_row vc_row-fluid vc_custom_1495196100257 vc_row-has-fill sc_layouts_row sc_layouts_row_type_normal sc_layouts_hide_on_frontpage scheme_dark">
+            <div className="wpb_column vc_column_container vc_col-sm-12 sc_layouts_column sc_layouts_column_align_center sc_layouts_column_icons_position_left">
+              <div className="vc_column-inner">
+                <div className="wpb_wrapper">
+                  <div id="sc_content_170140230" className="sc_content color_style_default sc_content_default sc_float_center sc_content_width_1_1">
+                    <div className="sc_content_container">
+                      <div className="sc_layouts_item">
+                        <div id="sc_layouts_title_851563643" className="sc_layouts_title">
+                          <div className="sc_layouts_title_title">
+                            <h1 className="sc_layouts_title_caption">Home</h1>
+                          </div>
+                          <div className="sc_layouts_title_breadcrumbs" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      );
+
+}
+
+export default Header;
